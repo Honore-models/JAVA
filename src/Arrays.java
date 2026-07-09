43c133ebb13eecb9fb24b6249c009eb6420ce8e2
honore <honoreniyogushimwa63@gmail.com>
2026-07-09 18:14:03 +0200

    Added arrays

---
+++ src/Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        String[] fruits = {"Mango", "Orange", "Apple"};
+
+        for(String fruit : fruits){
+            System.out.print(fruit + " ");
+        }
+    }
+}
--- src/Methods.java
+++ src/Methods.java
@@ -7,9 +7,27 @@ public class Methods {
         int area = area(width, height);
         System.out.println("Area is " + area);
 
+        String firstName = "NIYOGUSHIMWA";
+        String lastName = "Honore";
+
+        String fullName = fullName(firstName, lastName);
+        System.out.println("Full name is " + fullName);
+
     }
 
     static int area(int width, int height){
         return width * height;
     }
+
+    static String fullName(String firstName, String lastName){
+        return firstName + " " + lastName;
+    }
 }
+
+// Static methods and variables are variables and methods that belong to a class itself not a specific
+// instance of a class , when you create a static method it is to say that that method will remain the same
+// for all instances that will be created from that class i.e. all instances  of that class will use the
+// same method unchanged.
+
+// The static method can only use static methods and variables because it cannot access self keyword
+// which is only used by dynamic methods to access other methods.
