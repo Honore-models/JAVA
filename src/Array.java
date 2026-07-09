3532fd84adbab031f3e860e4914e2839245bb808
honore <honoreniyogushimwa63@gmail.com>
2026-07-09 19:32:54 +0200

    added arrays

--- src/Array.java
+++ src/Array.java
@@ -1,11 +1,17 @@
-public class Arrays {
+import java.util.Arrays;
+
+public class Array {
 
     public static void main(String[] args) {
 
         String[] fruits = {"Mango", "Orange", "Apple"};
 
+        Arrays.sort(fruits);
+
         for(String fruit : fruits){
             System.out.print(fruit + " ");
         }
+
+
     }
 }
