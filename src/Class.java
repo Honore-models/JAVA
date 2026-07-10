498656c86d87ce57042df96bc5a75c74e431133e
honore <honoreniyogushimwa63@gmail.com>
2026-07-10 15:26:55 +0200

    Added static methods and properties

--- src/Class.java
+++ src/Class.java
@@ -2,13 +2,27 @@ public class Class {
 
     public static void main(String[] args) {
 
-        Student student1 = new Student("Honore", "kayonza", 19);
-        Student student2 = new Student("Fabrice", "Kigali", 13);
+//        Student student1 = new Student("Honore", "kayonza", 19);
+//        Student student2 = new Student("Fabrice", "Kigali", 13);
+//
+//        System.out.println(student1.name);
+//        System.out.print(student2.name);
+//
+//        student2.study();
+//        student1.study();
+//
+//
+//        Student[] students = {student1, student2};
+//
+//        for(Student student : students){
+//            student.study();
+//        }
 
-        System.out.println(student1.name);
-        System.out.print(student2.name);
-        student2.study();
-        student1.study();
+        Static friend1 = new Static("Honore");
+        Static friend2 = new Static("Patrick");
+        Static friend3 = new Static("Fabrice");
+
+        System.out.println(Static.numOfFriends);
     }
 }
 
---
+++ src/Static.java
@@ -0,0 +1,10 @@
+public class Static {
+
+    static int numOfFriends = 0;
+    String name;
+
+    Static(String name){
+        this.name = name;
+        numOfFriends++;
+    }
+}
