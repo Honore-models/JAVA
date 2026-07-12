b45f06083fd53adacb7e07350e8d8287f8de4366
honore <honoreniyogushimwa63@gmail.com>
2026-07-12 20:17:03 +0200

    feat: added array lists

---
+++ src/ArrayLists.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class ArrayLists {
+
+    public static void main(String[] args) {
+
+//        ArrayLists are dynamic arrays unlike normal arrays that don't change in size
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<String> fruits = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        fruits.add("Apple");
+        fruits.add("Orange");
+        fruits.add("Watermelon");
+        fruits.add("Pear");
+
+        fruits.remove(3);
+
+        System.out.println(numbers);
+        System.out.println(fruits);
+    }
+}
