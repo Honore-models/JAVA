6bc60038cb580f851819c55d45361c627ad8402a
honore <honoreniyogushimwa63@gmail.com>
2026-07-12 06:25:45 +0200

    feat: added interfaces

---
+++ src/Interfaces.java
@@ -0,0 +1,12 @@
+public class Interfaces {
+
+    public static void main(String[] args) {
+
+//        Interfaces are somehow similar to abstract classes cz they define methods that should be implemented in
+//        the class that inherits from it, but the difference between interfaces and abstract classes they support
+//        a subclass to inherit from multiple parents unlike abstract  classes that don't
+
+    }
+}
+
+
---
+++ src/Rabbit.java
@@ -0,0 +1,6 @@
+public class Rabbit implements Prey{
+
+    public void run(){
+        System.out.println("I am a prey escaping a predator");
+    }
+}
