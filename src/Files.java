ce6a029961d721d3c01cfc4a6924715ec320319d
honore <honoreniyogushimwa63@gmail.com>
2026-07-13 09:14:59 +0200

    feat

--- src/Files.java
+++ src/Files.java
@@ -5,6 +5,12 @@ public class Files {
 
     public static void main(String[] args){
 
+//       There are different ways of interacting with files in Java depending on what you have:
+//        1. FileWritter : This is used when you have small files like text files
+//        2. BufferWriter : When you are dealing with large files
+//        3. PrintWriter : When you are dealing with structured files
+
+
 //       ➡️ This is ordinary method of opening a file, but there is the best approach called try-with-resources
 //       ⚠️ If you try to write a file without closing it , it may cause data and resource leakage.
 
