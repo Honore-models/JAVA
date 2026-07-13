b61b91fbfd3ee62e06e34168704e35c97532d5e2
honore <honoreniyogushimwa63@gmail.com>
2026-07-13 08:44:26 +0200

    feat

--- src/Files.java
+++ src/Files.java
@@ -1,2 +1,16 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Files {
+
+    public static void main(String[] args){
+
+        try{
+            FileWriter file = new FileWriter("text.txt");
+            file.write("Hello world");
+            System.out.println("File was written successfully");
+        }catch(IOException e){
+            System.out.println("Could not open file.");
+        }
+    }
 }
