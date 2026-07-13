6376ae96719b59a156aa29e85c444be3a8ee6b41
honore <honoreniyogushimwa63@gmail.com>
2026-07-13 09:11:33 +0200

    feat: writting files with try-with-resources approach

--- src/Files.java
+++ src/Files.java
@@ -5,12 +5,35 @@ public class Files {
 
     public static void main(String[] args){
 
-//      this is try-with-resources approach that closes the file automatically.
-        try(FileWriter file = new FileWriter("test.txt");){
+//       ➡️ This is ordinary method of opening a file, but there is the best approach called try-with-resources
+//       ⚠️ If you try to write a file without closing it , it may cause data and resource leakage.
 
-            file.write("\nThis is my first file.\n");
+        FileWriter file = null;
+
+        try{
+            file = new FileWriter("test.txt");
+            file.write("This is my first file.\n");
             System.out.println("File was written successfully");
 
+        }catch(IOException e){
+            System.out.println("Could not open file.");
+
+        }finally {
+
+            if(file != null){
+                try {
+                    file.close();
+                } catch (IOException e) {
+                    System.out.println("Could not close file.");
+                }
+            }
+        }
+
+//      This is try-with-resources approach that closes the file automatically.
+
+        try(FileWriter file2 = new FileWriter("with-resources.txt")){
+            file2.write("This is the second file with try-with-resources approach");
+            System.out.println("File2 written successfully");
         }catch(IOException e){
             System.out.println("Could not open file.");
         }
