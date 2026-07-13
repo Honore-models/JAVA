c25f263266662970e526fd265c3a733c8bc2e7b2
honore <honoreniyogushimwa63@gmail.com>
2026-07-13 12:40:57 +0200

    feat: added a reading file

--- src/OpenFile.java
+++ src/OpenFile.java
@@ -6,10 +6,27 @@ import java.io.IOException;
 public class OpenFile {
 
     public static void main(String[] args) {
-//        When reading files in Java we combine bufferedReader and FileReader
 
-        try(BufferedReader reader = new BufferedReader(new FileReader("with-resources.txt"))){
-            System.out.println("File exists and was opened successfully.");
+//        When reading files in Java we combine bufferedReader and FileReader
+//        💡 What is Buffering? Buffering is the process by which the data is stored to temporary memory (RAM) instead of
+//        Moving direct from source to destination.
+
+//       ➡️ How BufferReader and FileReader works together to improve performance and efficiency
+
+//        The file reader reads the file and then the buffer reader get that content from file reader and stores them in
+//        the Buffer and this reduce the number of reads from that file because when the contents are needed again it
+//        will come from Buffer
+
+        String filePath = "C:\\Users\\HP\\Documents\\KiCad\\DB_PROMPT.txt";
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+
+            String line;
+
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+
         }catch(FileNotFoundException e){
             System.out.println("File not found.");
         }catch(IOException e){
