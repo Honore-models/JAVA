4ca9197ced79c301131aa62f36712b4c62d3ab18
honore <honoreniyogushimwa63@gmail.com>
2026-07-13 09:18:38 +0200

    refactor:

--- src/Files.java
+++ src/Files.java
@@ -6,7 +6,7 @@ public class Files {
     public static void main(String[] args){
 
 //       There are different ways of interacting with files in Java depending on what you have:
-//        1. FileWritter : This is used when you have small files like text files
+//        1. FileWriter : This is used when you have small files like text files
 //        2. BufferWriter : When you are dealing with large files
 //        3. PrintWriter : When you are dealing with structured files
 
