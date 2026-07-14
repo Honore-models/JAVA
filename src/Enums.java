61bac36fd1cca4108ab45833dd61aba4f35c1589
honore <honoreniyogushimwa63@gmail.com>
2026-07-14 14:30:46 +0200

    feat: added enums

---
+++ src/Enums.java
@@ -0,0 +1,13 @@
+public class Enums {
+
+    public static void main(String[] args) {
+
+//        Enums in Java are special kind of Classes that represent a fixed set of constant values.
+//        They are mostly used when we want to make a variable only assignable to a given set of values
+//        NOTE: Enums are like just creating your own Datatype, like other java datatypes like String which means that
+//        that variable can hold only strings , and enum datatype shows that that variable can hold only one value among those
+//        defined in the enum
+
+
+    }
+}
